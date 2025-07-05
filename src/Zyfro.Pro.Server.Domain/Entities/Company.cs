e842efa6d52b8ebd3db1ca10733dce3d96164bc4
Sead Xhigoli <sead.xhigoli1@gmail.com>
2025-07-05 11:55:14 +0200

    Update company entity

--- src/Zyfro.Pro.Server.Domain/Entities/Company.cs
+++ src/Zyfro.Pro.Server.Domain/Entities/Company.cs
@@ -9,12 +9,5 @@ namespace Zyfro.Pro.Server.Domain.Entities
         public string Name { get; set; }
         public ICollection<ApplicationUser> ApplicationUsers { get; set; }
         public ICollection<Document> Documents { get; set; }
-
-        public Company()
-        {
-            Id = Guid.NewGuid();
-            CreatedAtUtc = DateTime.UtcNow;
-            UpdatedAtUtc = DateTime.UtcNow;
-        }
     }
 }
